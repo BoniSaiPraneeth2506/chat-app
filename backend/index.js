e3fe7f8615107b10f98607874fb6c49b8d25b797
Sai Praneeth Boni <saipraneethboni2506@gmail.com>
2025-06-26 23:36:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import express from 'express'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import connectDB from './lib/db.js';
-
+import path from "path";
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import { app, server } from './lib/socket.js';
@@ -19,13 +19,19 @@ app.use(cors({
   origin:"http://localhost:5173",
   credentials:true
 }))
+const __dirname = path.resolve();
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get('/',(req,res)=>{
-    res.send("api is working")
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
 const PORT =process.env.PORT
 server.listen(PORT,()=>{
     console.log("server running on port 5001")
