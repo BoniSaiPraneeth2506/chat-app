fd26b097046f617b49e1114a962509569570143e
Sai Praneeth Boni <saipraneethboni2506@gmail.com>
2025-06-27 13:19:19 +0530

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,7 +17,7 @@ const NoChatSelected = () => {
 
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
-        <p className="text-base-content/60">
+        <p className="text-base-content/60 mb-3">
           Select a conversation from the sidebar to start chatting
         </p>
       </div>
